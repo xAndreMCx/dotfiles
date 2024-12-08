cf23520fd8ee7f12c7c40d9234345efa225ccbfb
Andre du Toit <xandremcx@gmail.com>
2024-12-08 14:14:23 +0200

    seperate wezterm config into multiple files

---
+++ wezterm/keybinds.lua
@@ -0,0 +1,36 @@
+local wezterm = require('wezterm')
+local config = {}
+config.disable_default_key_bindings = true
+config.leader = { key = ' ', mods = 'CTRL', timeout_milliseconds = 5000 }
+config.keys = {
+  {
+    key = 't',
+    mods = 'LEADER',
+    action = wezterm.action.SpawnTab('CurrentPaneDomain'),
+  },
+  {
+    key = 'q',
+    mods = 'LEADER',
+    action = wezterm.action.CloseCurrentPane({ confirm = false }),
+  },
+  {
+    key = 'h',
+    mods = 'LEADER',
+    action = wezterm.action.SplitHorizontal({ domain = 'CurrentPaneDomain' }),
+  },
+  {
+    key = 'v',
+    mods = 'LEADER',
+    action = wezterm.action.SplitVertical({ domain = 'CurrentPaneDomain' }),
+  },
+}
+
+for i = 1, 8 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = 'CTRL',
+    action = wezterm.action.ActivateTab(i - 1),
+  })
+end
+
+return config
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,15 +1,28 @@
-local wezterm = require("wezterm")
+local wezterm = require('wezterm')
+
+local function merge_tables(...)
+  local merged = {}
+  for _, tbl in ipairs({ ... }) do
+    for key, value in pairs(tbl) do
+      merged[key] = value
+    end
+  end
+  return merged
+end
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = 'Catppuccin Mocha'
 config.font_size = 8
-config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font = wezterm.font('JetBrainsMono Nerd Font')
 
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.show_new_tab_button_in_tab_bar = false
 
-config.default_cursor_style = "BlinkingBar"
-config.audible_bell = "Disabled"
+config.default_cursor_style = 'BlinkingBar'
+config.audible_bell = 'Disabled'
+
+config = merge_tables(config, require('keybinds'))
+
 return config
