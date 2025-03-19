87da8aa6377173cb52d405b40b8c16356a319bf1
Andre du Toit <xandremcx@gmail.com>
2025-03-19 20:10:29 +0200

    add workspaces and datetime modules

--- ignis/config.py
+++ ignis/config.py
@@ -1,13 +1,24 @@
-from ignis.widgets import Widget
 from ignis.app import IgnisApp
-from ignis.utils import Utils
-
+from ignis.app import Utils
+from ignis.services.hyprland import HyprlandService
+import json
 from modules.bar.bar import bar
 
 app = IgnisApp.get_default()
-app.apply_css("./styles/style.scss")
+# app.apply_css("./styles/style.scss")
 
-for n in range(Utils.get_n_monitors()):
-    bar(n)
+hyprland = HyprlandService.get_default()
+# hyprland.connect("notify::active-workspace", lambda x, y: print(x, y))
+monitors_list = json.loads(hyprland.send_command("j/monitors"))
+MONITORS = [
+    {
+        "hyprland_id": item["id"],
+        "id": 0 if item["name"] == "DP-1" else 1,
+        "name": item["name"],
+    }
+    for item in monitors_list
+]
 
-
+# for n in range(Utils.get_n_monitors()):
+for n in range(2):
+    bar(MONITORS[n])
--- ignis/modules/bar/bar.py
+++ ignis/modules/bar/bar.py
@@ -1,38 +1,63 @@
 import datetime as dt
 from ignis.widgets import Widget
 from ignis.utils import Utils
+from ignis.services.hyprland import HyprlandService
+import os
+from typing import Dict
+
+hyprland = HyprlandService.get_default()
+
 
 def update_datetime(self: Widget.Label) -> None:
-    date = dt.datetime.now().strftime("%A, %d %b %Y") 
-    time = dt.datetime.now().strftime("%I:%M %p") 
+    date = dt.datetime.now().strftime("%A, %d %b %Y")
+    time = dt.datetime.now().strftime("%I:%M %p")
     self.label = f" {date} |  {time}"
 
+
 def datetime() -> Widget.Box:
+    return Widget.Box(css_classes=["center_box module"], child=[Widget.Label()])
+
+
+def workspace_dispatch(monitor: Dict[str, int], workspace_id: int):
+    workspace = workspace_id + 10 * monitor["id"]
+    # os.system(
+    #     f"notify-send {monitor['name']},hypr={monitor['hyprland_id']}, id={monitor['id']}{workspace=}"
+    # )
+    hyprland.send_command(f"dispatch workspace {workspace}")
+
+
+def workspaces(monitor: Dict[str, int]) -> Widget.Box:
     return Widget.Box(
-        css_classes = ["center_box module"],
-        child = [Widget.Label()]
+        css_classes=["workspaces"],
+        child=[
+            Widget.Button(
+                css_classes=["workspace_btn"],
+                child=Widget.Label(label=str(i)),
+                on_click=lambda x: workspace_dispatch(monitor, int(x.child.label)),
+            )
+            for i in range(1, 11)
+        ],
     )
 
 
-def bar(monitor: int) -> Widget.Window:
+def bar(monitor: Dict[str, int]) -> Widget.Window:
     date_time = datetime()
     Utils.Poll(1000, lambda x: update_datetime(date_time.child[0]))
 
     bar = Widget.Window(
-        namespace = f"bar_monitor{monitor}",
-        monitor = monitor,
-        anchor = ["top", "left", "right"],
-        exclusivity = "exclusive",
-        layer = "top",
-        margin_left = 10,
-        margin_right = 10,
-        margin_top = 3,
-        child = Widget.CenterBox(
-            css_classes = ["bar"],
-            start_widget= Widget.Label(label = "Hello"),
+        namespace=f"bar_monitor{monitor}",
+        monitor=monitor["hyprland_id"],
+        anchor=["top", "left", "right"],
+        exclusivity="exclusive",
+        layer="top",
+        margin_left=10,
+        margin_right=10,
+        margin_top=3,
+        child=Widget.CenterBox(
+            css_classes=["bar"],
+            start_widget=workspaces(monitor),
             center_widget=date_time,
-            end_widget=Widget.Label(label = "Hello")
-        )
-    ) 
+            end_widget=Widget.Label(label="Hello"),
+        ),
+    )
     return bar
-
