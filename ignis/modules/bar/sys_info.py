e37d244c4796605f0e3f05f0ff84ce261e0bb49c
Andre du Toit <xandremcx@gmail.com>
2025-07-09 23:23:46 +0200

    add sys tray

--- ignis/modules/bar/sys_info.py
+++ ignis/modules/bar/sys_info.py
@@ -1,11 +1,53 @@
+import asyncio
 from ignis.widgets import Widget
 from ignis.app import IgnisApp
 from ignis.services.upower import UPowerService
+from ignis.services.system_tray import SystemTrayService, SystemTrayItem
+from ignis.services.network import NetworkService
 
 from modules.volume import volume_button
 
 app = IgnisApp.get_default()
 power_service = UPowerService().get_default().batteries[0]
+system_tray = SystemTrayService.get_default()
+network = NetworkService.get_default()
+
+
+class TrayItem(Widget.Button):
+    __gtype_name__ = "TrayItem"
+
+    def __init__(self, item: SystemTrayItem):
+        if item.menu:
+            menu = item.menu.copy()
+        else:
+            menu = None
+
+        super().__init__(
+            child=Widget.Box(
+                child=[
+                    Widget.Icon(image=item.bind("icon"), pixel_size=24),
+                    menu,
+                ]
+            ),
+            tooltip_text=item.bind("tooltip"),
+            on_click=lambda x: asyncio.create_task(item.activate_async()),
+            setup=lambda self: item.connect("removed", lambda x: self.unparent()),
+            on_right_click=lambda x: menu.popup() if menu else None,
+            css_classes=["tray-item", "unset"],
+        )
+
+
+class Tray(Widget.Box):
+    __gtype_name__ = "Tray"
+
+    def __init__(self):
+        super().__init__(
+            css_classes=["tray"],
+            setup=lambda self: system_tray.connect(
+                "added", lambda x, item: self.append(TrayItem(item))
+            ),
+            spacing=10,
+        )
 
 
 def ram() -> Widget.Box:
@@ -19,7 +61,7 @@ def cpu() -> Widget.Box:
 def battery() -> Widget.Box:
     return Widget.Box(
         css_classes=[""],
-        spacing=5,
+        spacing=2,
         child=[
             Widget.Icon(image="battery_full-symbolic"),
             Widget.Label(
@@ -32,8 +74,8 @@ def battery() -> Widget.Box:
 def wifi() -> Widget.Button:
     return Widget.Button(
         css_classes=["wifi_button", "bar_button"],
-        child=Widget.Icon(image="wifi-symbolic"),
-        on_click=lambda self: app.toggle_window(),
+        child=Widget.Icon(image=network.wifi.bind("icon_name", lambda x: x)),
+        on_click=lambda self: app.toggle_window("wifi_menu"),
     )
 
 
@@ -41,17 +83,18 @@ def bluetooth() -> Widget.Button:
     return Widget.Button(
         css_classes=["bluetooth_button", "bar_button"],
         child=Widget.Icon(image="bluetooth-symbolic"),
-        on_click=lambda self: app.toggle_window(),
+        on_click=lambda self: app.toggle_window("bluetooth_menu"),
     )
 
 
 def sys_info() -> Widget.Box:
     return Widget.Box(
-        css_classes=[""],
-        spacing=10,
+        css_classes=["sys_info"],
+        spacing=5,
         child=[
-            ram(),
-            cpu(),
+            Tray(),
+            # ram(),
+            # cpu(),
             battery(),
             wifi(),
             bluetooth(),
